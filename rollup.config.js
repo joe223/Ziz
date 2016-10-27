c87dd47d2ce6e2c5660462ad7b2ec51209c5fdfc
joe223 <heyin223@163.com>
2016-10-27 14:55:44 +0800

    use rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,30 @@
 import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
-import npm from "rollup-plugin-node-resolve";
+import npm from 'rollup-plugin-node-resolve';
+import eslint from 'rollup-plugin-eslint';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-    entry: "./src/blade.js",
-    plugins: [ json(), babel(), npm({jsnext : true}), commonjs() ],
-    moduleName : "blade",
-    dest: "./dist/blade.js",
-    format : "umd",
+    entry: './src/blade.js',
+    plugins: [
+        json(),
+        babel({
+            exclude: 'node_modules/**',
+        }),
+        npm({
+            jsnext : true
+        }),
+        eslint({
+            exclude: [
+                'test/**',
+                'dist/**',
+                'node_modules/**'
+            ]
+        }),
+        commonjs()
+    ],
+    moduleName : 'blade',
+    dest: './dist/blade.js',
+    format : 'umd',
     sourceMap: true
-
-    // format: 'umd'
 };
--- src/blade.js
+++ src/blade.js
@@ -5,3 +5,7 @@ export default ( content ) => {
     content = header( content );
     return content;
 };
+
+if ( process.env.NODE_ENV === "development" ) {
+    
+}
